e2bf07c33a395199760ef7e94483891f69d64d0d
Göran Ahlgren <48514421+GorAhl@users.noreply.github.com>
2024-05-30 08:29:20 +0200

    Lade tillbaka BeforEch och flyttade upp Product variabelnerna till den. Blir färre rader kod på detta sett. Skrev två nya enhetstester modul.product.setPrice och modul.product.getPrice

--- src/test/java/com/example/produktapi/model/ProductTest.java
+++ src/test/java/com/example/produktapi/model/ProductTest.java
@@ -1,5 +1,6 @@
 package com.example.produktapi.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,9 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ProductTest {
 
+    Product product;
+    @BeforeEach
+    void setUp(){
+        product = new Product();
+    }
     @Test
     void getId_ReturnsCorrectId() {
-        Product product = new Product();
         Integer expectedId = 1;
         product.setId(expectedId);
         Integer actualId = product.getId();
@@ -17,16 +22,32 @@ public class ProductTest {
     }
     @Test
     void getId_ReturnsNullWhenIdNotSet() {
-        Product product = new Product();
         Integer actualId = product.getId();
         assertNull(actualId, "The ID should be null when it is not set");
     }
 
     @Test
     void getTitle(){
-        Product product = new Product();
         product.setTitle("Product title");
         assertEquals(product.getTitle(), "Product title");
+    }
+
+    @Test
+    void setPrice(){
+        Double firstPrice = 12.34;
+        Double newPrice = 56.78;
+        product.setPrice(firstPrice);
+        assertEquals(product.getPrice(), firstPrice);
+        product.setPrice(newPrice);
+        assertEquals(product.getPrice(), newPrice);
+    }
+
+    @Test
+    void getPrice(){
+        assertNull(product.getPrice(), "The price should be null when it is not set");
+        Double expectedPrice = 12.34;
+        product.setPrice(expectedPrice);
+        assertEquals(product.getPrice(), expectedPrice);
     }
 }
 
