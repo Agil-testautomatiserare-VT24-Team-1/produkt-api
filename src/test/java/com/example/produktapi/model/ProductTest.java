0f3c322cf110a7c7862e61e100a0bf25b17839db
Göran Ahlgren <48514421+GorAhl@users.noreply.github.com>
2024-05-30 08:16:34 +0200

    Städade bort Befor och after funktionerna Ändrade lite radbrytningar

--- src/test/java/com/example/produktapi/model/ProductTest.java
+++ src/test/java/com/example/produktapi/model/ProductTest.java
@@ -1,7 +1,5 @@
 package com.example.produktapi.model;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,39 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ProductTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void getAllProducts() {
-    }
     @Test
     void getId_ReturnsCorrectId() {
-
         Product product = new Product();
         Integer expectedId = 1;
         product.setId(expectedId);
-
-
         Integer actualId = product.getId();
-
-
         assertEquals(expectedId, actualId);
     }
     @Test
     void getId_ReturnsNullWhenIdNotSet() {
-
         Product product = new Product();
-
-
         Integer actualId = product.getId();
-
-
         assertNull(actualId, "The ID should be null when it is not set");
     }
 
