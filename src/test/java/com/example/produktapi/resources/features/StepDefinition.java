6d9e0d03b8557825bbdb79a5f884784364c8207a
Göran Ahlgren <48514421+GorAhl@users.noreply.github.com>
2024-06-24 18:14:06 +0200

    Cleaned up some whitespace, tabs, commented-out code, and inactive imports. One more

--- src/test/java/com/example/produktapi/resources/features/StepDefinition.java
+++ src/test/java/com/example/produktapi/resources/features/StepDefinition.java
@@ -37,7 +37,7 @@ public class StepDefinition {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
